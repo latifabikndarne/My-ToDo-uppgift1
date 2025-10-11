672afbd316a1fe38ca5430224b083d3f012d2c1e
latifabikndarne <latifa.bikndarne@gmail.com>
2025-10-11 10:12:09 +0200

    Some changes in css file

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ itemText.addEventListener("click",
 
 // att lägga till papperskorgsikon
     const trashcan = document.createElement("span");
-    trashcan.innerHTML = "&#128465;" 
+   trashcan.innerHTML = "&#128465;"
     trashcan.setAttribute("class", "trashcan");
     item.appendChild(trashcan);
 
